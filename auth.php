2d91af6e8aed825d1429d9283e5397b82276e248
douira <douira100@gmail.com>
2016-12-04 15:59:43 +0100

    swapped names

--- auth.php
+++ auth.php
@@ -1,19 +1,20 @@
 <?php
 include "util.php";
-setupPage("Authentifizierung");
+setupPage("Anmelden");
 
 if (permIsHigh($userPerm)) {
   makeAlert("Schon authentifiziert", "info", "Info:");
 } else if (! permIsHigh(adminGetPerm($userId))) {
   makeAlert("Benutzer ist nicht Administrator und kann nicht authentifiziert werden.", "warning", "Warnung:");
 } else {
-  echo "<h3>Anmelden:</h3>";
+  echo "<h3>Authentifizierung:</h3>";
   if (empty($_GET["backto"])) {
     $_GET["backto"] = "messages.php";
   }
   echo "<form action='{$_GET["backto"]}' method='post'><div class='form-group'>
     <label for='userName'>Benuztername</label>
     <input type='text' class='form-control' name='userName' id='userName' value='{$userName}'>
+    <br>
     <label for='pswd'>Passwort</label>
     <input type='password' class='form-control' name='pswd' id='pswd' placeholder='Administratorpasswort'>
     <br>
